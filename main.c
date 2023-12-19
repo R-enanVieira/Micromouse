ee581cf487694d131aaa44a1ba2f1aded097f896
R-enanVieira <renanv.guedes7@gmail.com>
2023-12-19 16:01:59 -0300

    feat: add explorar logic

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "lib.c"
 
-#define esquerdinha 'l'  // → rotacionar 90º para a esquerda (sentido anti-horário).
+#define esquerdinha \
+  'l'  // → rotacionar 90º para a esquerda (sentido anti-horário).
 #define direitinha 'r'  // r → rotacionar 90º para a direita (sentido horário).
 #define frentinha 'w'   // → caminhar para frente. (anda uma casa)
 #define corridinha 'j'  // → corridinha para frente. (anda duas casas)
@@ -9,8 +10,15 @@
 #define sensor 'c'     // → ativar sensor de paredes próximas.
 #define distancia_euclides 'd'  // → ativar sensor de proximidade do objetivo.
 
-//iniciando a matrix
+// iniciando a matrix
 mapa grid[MAX_ROW][MAX_COL];
+
+// variavel que diz a direção que o rato está olhando
+int lookAt = 2;  // fala que ele nasce olhando pra esquerda
+
+// inicaliza nossa fila e stack
+head *queue;
+head *stack;
 
 int doAction(char c) {
   printf("%c\n", c);
@@ -23,52 +31,76 @@ int doAction(char c) {
   return ans;
 }
 
-bool explore(pair coord, char dir) {
-  int judgeAns = doAction(dir);
+bool explore(pair coord, char rDir) {
+  int judgeAns = doAction(rDir);
 
-  if(!grid[coord.p][coord.s].visitado)
-      return false;
+  if (!grid[coord.p][coord.s].visitado) return false;
+  // caso encontramos um nó que já foi visitado -> temo um looping
+  // desenfilelira e aplica o goBack até chegarmos na coordenada que
+  // desenfileiramos mudar a direcao pra andar pra tras
 
-  locomover({coord.p + 1, coord.s +1}, dir);
+  if (judgeAns == 0) {
+    setWall(grid, coord, lookAt);  // se move na matriz e mapeia aquela
+                                   // celula de acordo com o judgeAns;
 
-   //desempilha ate o topo da pilha = topo da lista
-   //andar pra tras
-   //mudar a direcao pra andar pra tras
-   //andar pra tras ate encontrar o inicio da fila
+    judgeAns = doAction(sensor);  // usa o sensor
 
+    // respostas do sensor (1 -> livre e 0 -> parede)
+    int frente = (judgeAns >> 0) & 1;
+    int direita = (judgeAns >> 1) & 1;
+    int tras = (judgeAns >> 2) & 1;
+    int esquerda = (judgeAns >> 3) & 1;
 
-  if(judeAns == 0)
-    //bateu na parede
-    //marcar na matrix que encontrou uma parede
-    //lembrar que a matrix anda antes de perguntar
-    //matrix do rato e o grid sao difetrntes
-    //usar o sensor de paredes
-    //rotacionar e chama a funcao explore
+    if (direita && esquerda) enfileira(queue, coord);
 
-  if(judeAns == 1)
-    //esta livre
-    //anda pra frente ate bater numa parede
-    //enquanto anda, empilha os nos
-    //a cada passo, usa sensor
-    //se achar no com intersecaao, emfileira
+    if (esquerda)
+      doAction(esquerdinha), lookAt = 0, explore(coord, frente);
+    else if (direita)
+      doAction(direitinha), lookAt = 1, explore(coord, frente);
+    else {
+      // beco sem saída
+      // desenfileirar a ultima intersecção e desempilhar até chegar nela(usar o
+      // goBack)
+    }
+    // bateu na parede
+    // marcar na matrix que encontrou uma parede
+    // usar o sensor de paredes
+    // rotacionar e chama a funcao explore
+  } else if (judgeAns == 1) {
+    pair reloaded_coord = locomover(grid, coord, lookAt);
+    empilha(stack, reloaded_coord);
 
-  if(judeAns == 2)
-    //achou a saida
-    //desempilha ate chegar no primeiro no
+    judgeAns = doAction(sensor);  // usa o sensor
 
+    // respostas do sensor (1 -> livre e 0 -> parede)
+    int frente = (judgeAns >> 0) & 1;
+    int direita = (judgeAns >> 1) & 1;
+    int tras = (judgeAns >> 2) & 1;
+    int esquerda = (judgeAns >> 3) & 1;
 
+    if ((frente + direita + esquerda) > 1) enfileira(queue, reloaded_coord);
 
-  int frente = (judgeAns >> 0) & 1;    // 1 (livre)
-  int direita = (judgeAns >> 1) & 1;   // 0 (parede)
-  int tras = (judgeAns >> 2) & 1;      // 1 (livre)
-  int esquerda = (judgeAns >> 3) & 1;  // 1 (livre)
+    explore(reloaded_coord, frente);
+    // esta livre
+    // anda pra frente ate bater numa parede
+    // enquanto anda, empilha os nos
+    // a cada passo, usa sensor
+    // se achar no com intersecaao, emfileira
+  }
+  if (judgeAns == 2) {
+    // achou a saida
+    // desempilha ate chegar no primeiro no
+    //
+  }
 }
 
 int main() {
-
+  queue = criar_queue();
+  stack = criar_stack();
 
   pair coord_inicial = {MAX_ROW / 2, MAX_COL / 2};
+
+  empilha(stack, coord_inicial);
+
   explore(coord_inicial, frentinha);
 }
-
-
